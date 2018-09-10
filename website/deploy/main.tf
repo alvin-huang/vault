096a98e056ed3d0333875330fe24239316cb88b6
Jeff Escalante <gh.je@mailhero.io>
2018-09-10 15:23:51 -0400

    terraform fmt

--- website/deploy/main.tf
+++ website/deploy/main.tf
@@ -67,11 +67,11 @@ resource "netlify_site" "main" {
   name = "${var.name}"
 
   repo {
-    repo_branch = "${var.github_branch}"
-    command = "cd website && bundle && middleman build --verbose"
+    repo_branch   = "${var.github_branch}"
+    command       = "cd website && bundle && middleman build --verbose"
     deploy_key_id = "${netlify_deploy_key.key.id}"
-    dir = "website/build"
-    provider = "github"
-    repo_path = "${local.github_full}"
+    dir           = "website/build"
+    provider      = "github"
+    repo_path     = "${local.github_full}"
   }
 }
--- website/deploy/variables.tf
+++ website/deploy/variables.tf
@@ -1,14 +1,14 @@
 variable "name" {
-  default = "vault-www"
+  default     = "vault-www"
   description = "Name of the website in slug format."
 }
 
 variable "github_repo" {
-  default = "hashicorp/vault"
+  default     = "hashicorp/vault"
   description = "GitHub repository of the provider in 'org/name' format."
 }
 
 variable "github_branch" {
-  default = "stable-website"
+  default     = "stable-website"
   description = "GitHub branch which netlify will continuously deploy."
 }
